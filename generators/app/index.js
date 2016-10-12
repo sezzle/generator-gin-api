dd619c41b89568122616052f7c4c3cfbd2a66d29
Killian Brackey <killian.brackey@sezzle.com>
2016-10-12 02:37:14 -0500

    Added fixes to spawn commands

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,34 +2,25 @@
 var yeoman = require('yeoman-generator');
 var mkdirp = require('mkdirp');
 
-// var chalk = require('chalk');
-// var yosay = require('yosay');
+var chalk = require('chalk');
+var yosay = require('yosay');
 
 module.exports = yeoman.Base.extend({
-  // prompting: function () {
-  //   // Have Yeoman greet the user.
-  //   this.log(yosay(
-  //     'Welcome to the terrific ' + chalk.red('generator-gin-api') + ' generator!'
-  //   ));
-  // },
-
-
-
-  prompting: function() {
-
+   prompting: function() {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the terrific ' + chalk.red('generator-gin-api') + ' generator!'
+    ));
     console.log('\n' +
-      '+-----------------------------------------+\n' +
-      '| G o | p r o j e c t | g e n e r a t o r |\n' +
-      '+-----------------------------------------+\n' +
+      '+------------------------------------------------+\n' +
+      '| G o  G I N | p r o j e c t | g e n e r a t o r |\n' +
+      '+------------------------------------------------+\n' +
       '\n');
-
-
-    // var cb = this.async();
 
     return this.prompt([{
       type: 'input',
       name: 'myappName',
-      message: 'What is the name of your application',
+      message: 'What is the name (or path ie myName/myProject if on github.com) of your application',
       default: 'testapp'
     }, {
       type: 'input',
@@ -40,9 +31,6 @@ module.exports = yeoman.Base.extend({
       this.log('app name', answers.myappName);
       this.myappName = answers.myappName;
       this.myrepoUrl = answers.myrepoUrl;
-      this.log()
-      this.log(answers.name)
-      // cb();
     }.bind(this));
   },
 
@@ -77,27 +65,23 @@ module.exports = yeoman.Base.extend({
     console.log("app name " + this.myappName)
     console.log(tmplContext)
 
-
     this.template('_main.go', 'main.go', tmplContext);
     // this.template('_README.md', 'README.md');
 
   },
 
+  // initialize git repo
   initializing: function () {
     this.spawnCommandSync('git', ['init', '--quiet']);
   },
 
+  // start govendor directory
   install: function () {
     this.spawnCommand('go', ['get']);
-    // this.spawnCommand('go', ['get', '-u', 'github.com/kardianos/govendor']);
-    this.spawnCommand('govendor', ['init', 'add +external']);
-    // this.spawnCommand('govendor', ['add +external']);
-
-    // this.composeWith('git-init', {}, {
-    // local: require.resolve('generator-git-init')
-    // });
-
-    // this.spawnCommand('git' ['init']);
+    this.spawnCommand('go', ['get', '-u', 'github.com/kardianos/govendor']);
+    // this.spawnCommand('go', ['get', '-u github.com/kardianos/govendor'])
+    this.spawnCommand('govendor', ['init']);
+    this.spawnCommand('govendor', ['add', '+external']);
   }
 
 });
